0a02d4c0506f951543ff44cab0ae3240527c1afb
Rari Raj <rariraj95@gmail.com>
2020-07-24 19:39:27 +0530

    extracted required data from website using beautifulsoup

--- covid19_livetracker_india.py
+++ covid19_livetracker_india.py
@@ -3,27 +3,34 @@ import requests
 import bs4
 import plyer
 
-#To give request to website for the required url
+
+# To give request to website for the required url
 def get_html_data(url):
     data = requests.get(url)
     return data
 
-def get_corona_details_india():
-    url="https://www.mohfw.gov.in/"
-    html_data=get_html_data(url)
-    #print(html_data.text)
 
+def get_corona_details_india():
+    url = "https://www.mohfw.gov.in/"
+    html_data = get_html_data(url)
+    # print(html_data.text)
+    bs = bs4.BeautifulSoup(html_data.text, 'html.parser')
+    # print(bs.prettify())
+    info_div=bs.find("div", class_="site-stats-count").find("ul").find_all("li")[0:4]
+    #print(info_div)
+    all_details=""
+    for item in info_div:
+        count=item.find("strong").get_text()
+        data_text=item.find("span").get_text()
+        all_details=all_details+data_text+":"+count+"\n"
+
+    return all_details
 
 
 
 def main():
-    get_corona_details_india()
+    print(get_corona_details_india())
 
 
 if __name__ == '__main__':
     main()
-
-
-
-
-
