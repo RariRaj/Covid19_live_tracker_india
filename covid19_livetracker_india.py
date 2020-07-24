f668cac254f1f9fa889b5b50388cf0ef8a5df881
Rari Raj <rariraj95@gmail.com>
2020-07-24 19:48:44 +0530

    window created using tkinter and added imade and corona details

--- covid19_livetracker_india.py
+++ covid19_livetracker_india.py
@@ -2,6 +2,7 @@
 import requests
 import bs4
 import plyer
+import tkinter as tk
 
 
 # To give request to website for the required url
@@ -16,16 +17,15 @@ def get_corona_details_india():
     # print(html_data.text)
     bs = bs4.BeautifulSoup(html_data.text, 'html.parser')
     # print(bs.prettify())
-    info_div=bs.find("div", class_="site-stats-count").find("ul").find_all("li")[0:4]
-    #print(info_div)
-    all_details=""
+    info_div = bs.find("div", class_="site-stats-count").find("ul").find_all("li")[0:4]
+    # print(info_div)
+    all_details = ""
     for item in info_div:
-        count=item.find("strong").get_text()
-        data_text=item.find("span").get_text()
-        all_details=all_details+data_text+":"+count+"\n"
+        count = item.find("strong").get_text()
+        data_text = item.find("span").get_text()
+        all_details = all_details + data_text + ":" + count + "\n"
 
     return all_details
-
 
 
 def main():
@@ -34,3 +34,20 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+window = tk.Tk()
+window.geometry("500x500")
+window.title("CORONA_LIVE_TRACKER_INDIA")
+window.config(background="yellow")
+
+# image label for window
+img = tk.PhotoImage(file='coronavirus.png')
+image_label = tk.Label(window, image=img, background="powder blue").pack()
+
+# for live data
+
+content_label = tk.Label(window, text=get_corona_details_india(),font=("Arial", 18, "bold"),
+                         background="powder blue")
+content_label.pack()
+
+window.mainloop()
